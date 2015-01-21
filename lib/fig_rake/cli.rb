f2017c710bb9e4ff87da088f3aeea4244c9cb272
Edward Paget <edward@cassetteta.pe>
2015-01-21 09:51:14 -0600

    Add frails command

--- lib/fig_rake/cli.rb
+++ lib/fig_rake/cli.rb
@@ -4,10 +4,10 @@ require 'fig_rake/command'
 
 module FigRake
   class Cli
-    def initialize(arguments)
+    def initialize(command, arguments)
       @options = Configuration.new
-      parse_arguments(arguments)
-      @command = Command.new(@options.container_name, @options.rake_args)
+      parse_arguments(command, arguments)
+      @command = Command.new(command, @options.container_name, @options.rake_args)
     end
 
     def exec
@@ -16,9 +16,9 @@ module FigRake
 
     private
     
-    def parse_arguments(args)
+    def parse_arguments(command, args)
       OptionParser.new do |opts|
-        opts.banner = "Usage: frake [options] [rake arguments]"
+        opts.banner = "Usage: f#{command} [options] [rake arguments]"
 
         opts.on('-c', '--container-name [NAME]', String,
                 "Name of Container to exec rake in") do |name|
--- lib/fig_rake/command.rb
+++ lib/fig_rake/command.rb
@@ -1,13 +1,13 @@
 module FigRake
   class Command
-    attr_accessor :container_name, :rake_args
+    attr_accessor :container_name, :rake_args, :command
     
-    def initialize(container_name, rake_args)
-      @container_name, @rake_args = container_name, rake_args
+    def initialize(command, container_name, rake_args)
+      @command, @container_name, @rake_args = command, container_name, rake_args
     end
 
     def exec
-      exit system("fig run --rm --entrypoint=rake #{container_name} #{rake_args.join(' ')}")
+      exit system("fig run --rm --entrypoint=#{command} #{container_name} #{rake_args.join(' ')}")
     end
   end
 end
